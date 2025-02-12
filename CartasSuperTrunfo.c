80e29e201d06f40385e4201534562159789fda72
MikeCS-wq <mike.csk@hotmail.com>
2025-02-12 02:22:49 +0000

    criei o jogo super trunfo paises

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,100 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Definindo a estrutura de dados para armazenar os dados de cada cidade
+
+typedef struct {
+    char codigo[4];             //código da cidade( A01, A02)
+    int populacao;              //População da cidade
+    float area;                 //Área da cidade
+    float pib;                  //PIB da cidade
+    int pontosTuristicos;       //Número de pontos turísticos
+
+} Cidade;
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    //Definição da quntidade de cidades a serem cadastradas
+    int numCidades = 8;  //número de cidades que iremos cadastrar
+    Cidade cidades[numCidades]; //array de cidades = vamos armazenas multiplas cidades
+
+//cadastro das cartas
+for(int i = 0; i < numCidades; i++) {
+    //informando numero da cidade
+    printf("Cadastro da cidade %d:\n", i + 1);
+
+    //lendo o código da cidade
+    printf("Digite o código da cidade (exemplo A01): ");
+    scanf("%s", cidades[i].codigo);
+
+    //lendo os dados da cidade
+    printf("Digite a população:");
+    scanf("%d", &cidades[i].populacao);
+
+    printf("Digite a àrea(em km):");
+    scanf("%f", &cidades[i].area);
+
+    printf("Digite o PIB (em milhões):");
+    scanf("%f", &cidades[i].pib)
+
+    printf("Digite o número de pontos turísticos:");
+    scanf("%d", cidades[i].pontosTuristicos);
+
+    printf("\n");       //linha em branco para separa os cadastros
+}
+
+//exibir os dados cadastrados pelo usuário
+printf("\nDados cadastrados:\n");
+for (int i = 0; i < numCidades; i++);
+    printf("Cidade %d - Código: %s\n", i + 1, cidades[i].codigo
+    printf("Populacao: %d\n", cidades[i].populacao);
+    printf("Área: %.2f km\n", cidades[i].area);
+    printf("PIB: %2f milhões\n", cidades[i].pib);
+    printf("Pontos turísticos: %d\n", cidades[i].pontosTuristicos);
+    printf("\n");
+
+ return 0;
+}
+
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+   
+
+
+//MENU DE OPÇÕES
+printf("SUPER TRUNFO PAíSES!");
+printf("--------------------");
+
+printf("ESCOLHA UMA OPÇÃO");
+printf("1 - INICIAR JOGO");
+printf("2 - REGRAS DO JOGO");
+printf("3 - SAIR DO JOGO");
+scanf("%d",&opcao);
+
+switch (opcao) {
+    
+    case 1:
+        printf("INICIANDO O JOGO...");
+
+
+
+}
+
+
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
